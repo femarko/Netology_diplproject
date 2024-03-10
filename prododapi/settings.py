1cf378bd12fce26e20f015e3da9b67d4357aa094
Femarko <fenstermark7@gmail.com>
2024-03-10 21:46:41 +0100

    Djoser implemented

--- prododapi/settings.py
+++ prododapi/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 
     'backend',
 ]
@@ -134,3 +136,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.TokenAuthentication',
+ ]
+}
